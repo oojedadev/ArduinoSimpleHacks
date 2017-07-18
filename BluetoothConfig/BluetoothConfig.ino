17d6c8a670237eeee0a61630459f6a797b71dd69
Oscar Ivan Ojeda Aldana <1315074@uqroo.mx>
2017-07-18 14:00:07 -0500

    Add files via upload

---
+++ BluetoothConfig/BluetoothConfig.ino
@@ -0,0 +1,16 @@
+#include <SoftwareSerial.h>
+SoftwareSerial MyBluetoothModule(10, 11);
+
+void setup() {
+  MyBluetoothModule.begin(9600);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (MyBluetoothModule.available()) {
+    Serial.write(MyBluetoothModule);
+  }
+  if (Serial.available()) {
+    MyBluetoothModule.write(Serial.read());
+  }
+}
